bf22873cbe15c03265650c3bdd568ecc73871505
Nikkes <nick.westerdijk@gmail.com>
2016-05-26 11:11:39 +0200

    movement

---
+++ Movement3D.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement3D : MonoBehaviour {
+
+    public float speed = 6.0F; 
+    public float speedDampTime = 0.1F;
+    public float gravity = 20.0F; 
+
+    private Vector3 moveDirection = Vector3.zero;
+    
+    void Update() {
+        CharacterController controller = GetComponent<CharacterController>();
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
