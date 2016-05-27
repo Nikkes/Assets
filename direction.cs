e6cac55908e8005c39893fd89e494b20477ebd41
Jesse Schoonveld <jesse.schoonveld@gmail.com>
2016-05-27 12:19:03 +0200

    AI patrol, Player movement, zwalken.

---
+++ direction.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class direction : MonoBehaviour {
+
+    public Transform target;
+    NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+
+        agent = GetComponent<NavMeshAgent>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+            agent.SetDestination(target.position);
+        else agent.ResetPath();
+
+	}
+}
