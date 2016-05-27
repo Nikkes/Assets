c9f4bdbca398acc1874bbb592ac5ba6a584b717a
Nikkes <nick.westerdijk@gmail.com>
2016-05-27 13:11:20 +0200

    pickup

---
+++ Pickup.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Pickup : MonoBehaviour{
+
+    public Text countText;
+
+    private int count;
+
+    void Start(){
+        count = 0;
+        SetCountText();
+    }
+
+    void Update(){
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag("Pick Up")){ //Set collectable as Pick Up in tag menu
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText(){
+        countText.text = "Count: " + count.ToString();
+    }
+}
